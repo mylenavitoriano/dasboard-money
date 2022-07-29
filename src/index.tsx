4cbc2b185cfd200f2f07257ba606b43c2ab118be
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2022-07-29 07:49:42 -0300

    limpado os dados que ficavam já adicionados no banco

--- src/index.tsx
+++ src/index.tsx
@@ -10,28 +10,7 @@ createServer({
     transaction: Model, 
   },
 
-  seeds(server){
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelancer de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2022-07-20 09:00:00')
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2022-07-22 13:20:00')
-        }
-      ]
-    })
-  },
+ 
 
   routes() {
     this.namespace = 'api';
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://dashboard-money.vercel.app/api',
+    baseURL: 'http://dashboard-money.vercel.app/api',
 })
