94c4c05cc9f60cc776faaedb0a0a479eb53d2963
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2022-07-29 08:39:27 -0300

    realizado condição paa mostrar a tabela só quando tiver dados

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -9,39 +9,41 @@ export function TransactionsTable () {
 
     return(
         <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {transactions.map(transaction => (
-                        <tr key={transaction.id}>
-                            <td>{transaction.title}</td>
-                            <td className={transaction.type}>
-                                {transaction.type == "withdraw" ? "- " : ""}
-                                {new Intl.NumberFormat('pt-BR', {
-                                    style: 'currency',
-                                    currency: 'BRL'
-                                }).format(transaction.amount)}
-                            </td>
-                            <td>{transaction.category}</td>
-                            <td>
-                                {new Intl.DateTimeFormat('pt-BR').format(
-                                    new Date(transaction.createdAt)
-                                )}
-                                
-                            </td>
+            {transactions && 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Valor</th>
+                            <th>Categoria</th>
+                            <th>Data</th>
                         </tr>
-                    ))}
+                    </thead>
 
-                </tbody>
-            </table>
+                    <tbody>
+                        {transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td>{transaction.title}</td>
+                                <td className={transaction.type}>
+                                    {transaction.type == "withdraw" ? "- " : ""}
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL'
+                                    }).format(transaction.amount)}
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>
+                                    {new Intl.DateTimeFormat('pt-BR').format(
+                                        new Date(transaction.createdAt)
+                                    )}
+                                    
+                                </td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+            }       
         </Container>
     )   
 }
